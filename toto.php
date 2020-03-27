aac930708cebb10875f91f150681caa6010cae58
pierrebrouard <pierrebrouard1997@gmail.com>
2020-03-27 10:36:54 +0100

    Update toto files + .gitignore

--- toto.php
+++ toto.php
@@ -1,2 +1,4 @@
-Bonjour, <?php echo htmlspecialchars($_POST['nom']); ?>.
-Tu as <?php echo (int)$_POST['age']; ?> ans.
+Hello, <?php echo htmlspecialchars($_POST['nom']); ?>.
+Tu as <?php echo (int)$_POST['age']; ?> ans. Et toi ?
+
+<?php echo (int)$_POST['age']; ?> ans.
